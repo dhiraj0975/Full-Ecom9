73a5fb47abc3dd2af451dfadfef975cfdf67ed47
Dhiraj Khobragade <brodj73@gmail.com>
2025-07-17 01:54:37 +0530

    remove localhost 5000

--- Admin/frontend/src/api.jsx
+++ Admin/frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://admin-backend-pied.vercel.app/api',
+  baseURL: 'https://admin-backend-six-steel.vercel.app/api',
     withCredentials: true, // ⬅️ yeh add karo
 });
 
