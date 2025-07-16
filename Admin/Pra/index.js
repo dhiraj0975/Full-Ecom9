db25cf8930fe77da9b13721aae5e81ce00c1afc2
Dhiraj Khobragade <brodj73@gmail.com>
2025-07-17 03:04:24 +0530

    c commit

--- Admin/Pra/index.js
+++ Admin/Pra/index.js
@@ -87,7 +87,7 @@ app.use((err, req, res, next) => {
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
