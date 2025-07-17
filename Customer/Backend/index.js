e77a36bd7b96a8e7153149a8815b40217889f19d
Dhiraj Khobragade <brodj73@gmail.com>
2025-07-18 01:58:50 +0530

    again redeploy

--- Customer/Backend/index.js
+++ Customer/Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS Configuration
 const corsOptions = {
-  origin: 'http://localhost:3001'  ,
+   origin: process.env.CORS_ORIGIN ,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
