5049f0320be459b3d85c125e2f2993cae1528a0f
Dhiraj Khobragade <brodj73@gmail.com>
2025-07-18 14:51:15 +0530

    axios fix

--- Customer/Frontend/src/api/cartApi.js
+++ Customer/Frontend/src/api/cartApi.js
@@ -1,25 +1,27 @@
-import axios from 'axios';
+import api from './axios';
+
+
 
 export const addToCart = async (productId, quantity = 1, price) => {
-  return axios.post('/api/cart/add', { product_id: productId, quantity, price }, {
+  return api.post('/api/cart/add', { product_id: productId, quantity, price }, {
     withCredentials: true
   });
 };
 
 export const getCart = async () => {
-  return axios.get('/api/cart', {
+  return api.get('/api/cart', {
     withCredentials: true
   });
 };
 
 export const removeFromCart = async (id) => {
-  return axios.delete(`/api/cart/remove/${id}`, {
+  return api.delete(`/api/cart/remove/${id}`, {
     withCredentials: true
   });
 };
 
 export const updateCartQuantity = async (id, quantity) => {
-  return axios.put(`/api/cart/update/${id}`, { quantity }, {
+  return api.put(`/api/cart/update/${id}`, { quantity }, {
     withCredentials: true
   });
 }; 
