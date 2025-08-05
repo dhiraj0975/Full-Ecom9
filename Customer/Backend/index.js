25dbbeb66787c52c8f478cc9ecf25d13fc8826e1
Dhiraj Khobragade <brodj73@gmail.com>
2025-08-05 11:49:01 +0530

    this is url fix

--- Customer/Backend/index.js
+++ Customer/Backend/index.js
@@ -24,6 +24,7 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
 
 
 
